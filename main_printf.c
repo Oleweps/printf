407127deddf90ea3c8795a118b66ae263f3ea749
manucho254 <manuchoadero@gmail.com>
2023-04-25 13:57:13 +0000

    fixed errors for file main_printf.c

--- main_printf.c
+++ main_printf.c
@@ -5,6 +5,35 @@
 #include <stdio.h>
 
 /**
+ * get_symbols - function that intializes ,
+ * an array of type_format
+ * @symbols: array of type_format to initialize
+ * Return: pointer to array of type_format
+ */
+type_format *get_symbols(type_format symbols[])
+{
+	type_format *tmp;
+
+	symbols[0].type = "char";
+	symbols[0].symbol = "%c";
+	symbols[1].type = "string";
+	symbols[1].symbol = "%s";
+	symbols[2].type = "percent";
+	symbols[2].symbol = "%%";
+	symbols[3].type = "integer";
+	symbols[3].symbol = "%d";
+	symbols[4].type = "integer";
+	symbols[4].symbol = "%i";
+	symbols[5].type = "binary";
+	symbols[5].symbol = "%b";
+	symbols[6].type = NULL;
+	symbols[6].symbol = NULL;
+
+	tmp = symbols;
+	return (tmp);
+}
+
+/**
  * _printf - function to print different values in terminal
  * @format: string to format
  * Return: the number of characters printed
@@ -15,23 +44,13 @@ int _printf(const char *format, ...)
 	va_list args;
 	unsigned int y, count = 0, len = _strlen(format);
 	char *symbol, *str;
+	type_format *symbols, tmp[7];
 
-	type_format symbols[] = {
-		{"char", "%c"},
-		{"string", "%s"},
-		{"percent", "%%"},
-		{"integer", "%d"},
-		{"integer", "%i"},
-		{"binary", "%b"},
-		{NULL, NULL}
-	};
-
+	symbols = get_symbols(tmp);
 	va_start(args, format);
-
 	/** pointer to hold the symbol we find in the format string */
 	symbol = malloc(sizeof(char) * 10);
 	str = malloc(sizeof(char) * (len + 1));
-
 	/** check for malloc failure */
 	if (str == NULL)
 	{
@@ -39,10 +58,7 @@ int _printf(const char *format, ...)
 	}
 	symbol[10] = '\0'; /** add null printer to the end */
 	_strcpy(str, format);
-
-	/**
-	 * loop over the format string
-	 */
+	/** loop over the format string */
 	for (y = 0;  str[y] != '\0'; y++)
 	{
 		/** we reset the data in the memory location symbol is pointing to */
@@ -60,7 +76,6 @@ int _printf(const char *format, ...)
 		/** function to print string characters */
 		count += print_character(y, str);
 	}
-
 	va_end(args);
 	free(symbol);
 
