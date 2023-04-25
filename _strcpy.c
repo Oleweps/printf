882fe2fdf929334c19e271ef3293568067d1855b
oleweps <weps6179@gmail.com>
2023-04-25 11:59:35 +0000

    fixed betty error mistakes

--- _strcpy.c
+++ _strcpy.c
@@ -1,21 +1,22 @@
 #include "main.h"
 
-/** 
+/**
  * _strcpy - function to print out a string
- * @str: the string array
+ * @src: the string array
+ * @dest: the string array
  * Return: pointer to new string
 */
 
 char *_strcpy(char *dest, const char *src)
 {
-    int x;
-    int len = _strlen(src);
+	int x;
+	int len = _strlen(src);
 
-    _memset(dest, 0 , _strlen(dest)); /** turn all values in string to zero*/
+	_memset(dest, 0, _strlen(dest)); /** turn all values in string to zero*/
 
-    for (x = 0; x <= len; x++)
-    {
-        dest[x] = src[x];
-    }
-    return (dest);
+	for (x = 0; x <= len; x++)
+	{
+		dest[x] = src[x];
+	}
+	return (dest);
 }
